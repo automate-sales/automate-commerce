4a6d1f7fdd726d244b7df8992079ad3b229d7ce4
NitinCraziino <nitin@craxinno.com>
2024-04-05 23:26:04 +0530

    add swiper slider

--- src/app/components/carousel.tsx
+++ src/app/components/carousel.tsx
@@ -3,6 +3,10 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/navigation'
 
 type Item = {
   imageUrl: string;
@@ -15,12 +19,14 @@ type CarouselProps = {
   items: Item[];
   autoplay?: number; // Autoplay speed in milliseconds for changing slides
   size?: 'sm' | 'md' | 'lg';
+  infiniteScroll?:boolean
 }
 
-const Carousel = ({ items, autoplay = 3000, size = 'md' }: CarouselProps) => {
+const Carousel = ({ items, autoplay = 3000, size = 'md', infiniteScroll= true }: CarouselProps) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const timeoutRef = useRef<NodeJS.Timeout | null>(null);
   const itemsRef = useRef([...items, ...items]); // Duplicate items for an infinite loop
+
 
   useEffect(() => {
     resetTimeout();
@@ -35,6 +41,7 @@ const Carousel = ({ items, autoplay = 3000, size = 'md' }: CarouselProps) => {
       resetTimeout();
     };
   }, [currentIndex, autoplay]);
+
 
   const resetTimeout = () => {
     if (timeoutRef.current) {
@@ -51,28 +58,43 @@ const Carousel = ({ items, autoplay = 3000, size = 'md' }: CarouselProps) => {
 
   return (
     <div className="w-full overflow-hidden p-8">
-      <div className={`flex transition-transform ease-linear duration-1000`} style={{ transform: `translateX(-${currentIndex * (itemSize.width + 32)}px)` }}>
-        {itemsRef.current.map((item, idx) => (
-          <div
-            className={`flex-none ${itemSize.padding} bg-white flex flex-col justify-center items-center text-center m-2 shadow-lg rounded-lg p-5`}
-            key={idx}
-            style={{ width: `${itemSize.width}px`, height: `${itemSize.height}px` }}
-          >
-            <Link className="flex items-center justify-center h-full" href={item.link || '#'}>
+      <div >
+        <Swiper
+          loop={infiniteScroll}
+          navigation
+          spaceBetween={20}
+          slidesPerView={6}
+          rewind
+          autoplay={{delay: autoplay}}
+          modules={[Navigation]}
+          onSwiper={swiper => console.log(swiper)}
+          className='h-96 w-full rounded-lg'
+        >
+          {itemsRef.current.map((item, idx) => (
+            <SwiperSlide key={idx}>
+              <div
+                className={`flex-none ${itemSize.padding} bg-white flex flex-col justify-center items-center text-center m-2 shadow-lg rounded-lg p-5`}
+                key={idx}
+                style={{ width: `${itemSize.width}px`, height: `${itemSize.height}px` }}
+              >
+                <Link className="flex items-center justify-center h-full" href={item.link || '#'}>
 
-                <Image
-                  src={item.imageUrl}
-                  alt={item.heading || `Carousel item ${idx}`}
-                  width={itemSize.width - 32} // Deduct padding for actual image size
-                  height={itemSize.height - 32} // Deduct padding for actual image size
-                  objectFit="contain"
-                />
+                  <Image
+                    src={item.imageUrl}
+                    alt={item.heading || `Carousel item ${idx}`}
+                    width={itemSize.width - 32} // Deduct padding for actual image size
+                    height={itemSize.height - 32} // Deduct padding for actual image size
+                    objectFit="contain"
+                  />
 
-            </Link>
-            <h2 className="text-sm font-semibold w-full">{item.heading}</h2>
-            <p className="text-xs">{item.subheading}</p>
-          </div>
-        ))}
+                </Link>
+                <h2 className="text-sm font-semibold w-full">{item.heading}</h2>
+                <p className="text-xs">{item.subheading}</p>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+
       </div>
     </div>
   );
