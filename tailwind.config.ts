06ebc9d229917b51d89c3b6eb8805e356c192bdd
NitinCraziino <nitin@craxinno.com>
2024-04-08 14:08:27 +0530

    fix pr comments

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+import plugin from 'tailwindcss/plugin';
 
 const config: Config = {
   content: [
@@ -18,6 +19,32 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addUtilities }) {
+      const newUtilities = {
+        '.slide-in-left': {
+          animation: 'slideInLeft 0.5s forwards',
+        },
+        '.slide-in-right': {
+          animation: 'slideInRight 0.5s forwards',
+        },
+        '.slider-next-btn': {
+          right: '-30px',
+        },
+        '.slider-prev-btn': {
+          left: '-30px',
+        },
+        '@keyframes slideInLeft': {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+        '@keyframes slideInRight': {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+      };
+      addUtilities(newUtilities); // Updated this line
+    }),
+  ],
 }
 export default config
