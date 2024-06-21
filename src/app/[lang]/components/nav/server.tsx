4ddf2ee5d2e2fe6bf9cb31ed38d50ce2f84a2e06
gkpty <gkardonski@gmail.com>
2024-06-21 15:46:08 -0500

    fix the cart icon in safari browser

--- src/app/[lang]/components/nav/server.tsx
+++ src/app/[lang]/components/nav/server.tsx
@@ -29,15 +29,15 @@ export const Brand = () => {
 
 
 export const ShoppingCart = async () => {
-    const cartLength = await getCartLength()
+    const cartLength = await getCartLength();
     return (
-        <div>
-            <Link className="relative" href="/cart">
+        <div className="relative flex items-center">
+            <Link href="/cart" className="relative inline-block">
                 <ShoppingCartIcon className="h-6 w-6" />
-                <span className="absolute inline-flex items-center justify-center w-5 h-5 text-xs font-bold text-white bg-red-500 bg-opacity-90 rounded-full -top-2 -end-2 dark:border-gray-900">
+                <span className="absolute inline-flex items-center justify-center w-5 h-5 text-xs font-bold text-white bg-red-500 bg-opacity-90 rounded-full -top-2 -right-2 dark:border-gray-900 border border-transparent">
                     {cartLength}
                 </span>
             </Link>
         </div>
-    )
-}
+    );
+};
